3d9b0846f00b9e628aa068d8856c8d67a3a1ee69
Ali Hasan <capt.3lii@gmail.com>
2024-02-19 15:00:57 +0300

    add the function that will get the company news from the api

--- get_news.py
+++ get_news.py
@@ -3,3 +3,16 @@ import requests
 from dotenv import load_dotenv
 
 load_dotenv()
+
+def get_comp_news(company_name):
+    # params used to structure the request to the API in a way that specifies exactly what information is being sought.
+    params = {
+        "engine": "google",
+        "tbm": "nws",
+        "q": company_name,
+        "api_key": os.environ["SERPAPI_API_KEY"],
+    }
+    response = requests.get('https://serpapi.com/search', params=params)
+    data = response.json()
+
+    return data.get('news_results')
