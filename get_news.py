e00517d1e3aa7fb4f3f75436fa375a384e5afda9
Ali Hasan <capt.3lii@gmail.com>
2024-02-19 15:35:42 +0300

    function that save the news in txt

--- get_news.py
+++ get_news.py
@@ -16,3 +16,17 @@ def get_comp_news(company_name):
     data = response.json()
 
     return data.get('news_results')
+
+filePath = "/Users/3lihasan/Desktop/test.txt"
+
+# function for saving the news in .txt file
+def save_news_txt(news, filename):
+    with open(filename, 'w') as file:
+        for news_item in news:
+            if news_item is not None:
+                title = news_item.get('title', 'No title')
+                link = news_item.get('link', 'No link')
+                date = news_item.get('date', 'No date')
+                file.write(f"Title: {title}\n")
+                file.write(f"Link: {link}\n")
+                file.write(f"Date: {date}\n\n")
