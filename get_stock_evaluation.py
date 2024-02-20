91878ddf05f8e601023f7839ee4230c2449ea33c
Ali Hasan <capt.3lii@gmail.com>
2024-02-20 14:34:54 +0300

    install yahooquery Pkg

--- get_stock_evaluation.py
+++ get_stock_evaluation.py
@@ -3,13 +3,13 @@ import yfinance as yf
 filePath = "/Users/3lihasan/Desktop/test.txt"
 # function for getting the evaluation of the stock selected
 def get_stock_evolution(company_name, period="1y"):
-    # Get the stock information
+    # Get the stock info
     stock = yf.Ticker(company_name)
 
-    # Get historical market data
+    # Get historical stock data
     hist = stock.history(period=period)
 
-    # Convert the DataFrame to a string with a specific format
+    # Convert the DataFrame to a string
     data_string = hist.to_string()
 
     # Append the string to the "test.txt" file
