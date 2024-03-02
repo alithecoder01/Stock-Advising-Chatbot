7e12a93de1130aefd588eaaf18eff2d6ea47f3a9
Ali Hasan <capt.3lii@gmail.com>
2024-03-02 17:01:38 +0300

    Update the system message of the second respons

--- analysing.py
+++ analysing.py
@@ -75,19 +75,13 @@ def analyse(request):
         model = ChatOpenAI(model="gpt-3.5-turbo", temperature=1)
         messages = [
             SystemMessage(
-                content=f"""Give  a detailled investment advice about {company_name} stock to answer
-                      the user request. Provide numbers about {company_name} company to justify
-                      your assertions, a lot ideally. Mention if you recommend to but or wait.
-                      Use the content provided in {content} to get the updated news and all the numbers of the company and give advice for short and long tirm.
-
-                      give a financial advice according to the given information in {content}.
-                      Never mention something like this:
-                      However, as with any investment, it is essential to conduct thorough research,
-                      assess your risk tolerance,
-                      and consider consulting with a financial advisor before making any investment decisions.
-
-                      Or somthing like: check with a financial anvistor.
-                      """
+                content=f"""Conduct a concise analysis that includes financial data—such as Stock Evolution, balance sheet, cash flow statement, and income statement—and key valuation measures like P/E, P/B ratios, and dividend yield for {company_name}.
+                    Assess the financial health, operational performance, and market valuation, integrating an evaluation of recent news to identify significant opportunities and risks.
+                    Summarize the findings to provide a clear investment recommendation advice, indicating whether investing in {company_name} is favorable or if caution is advised due to identified risks.
+                    Your analysis should offer straightforward investment advice based on the comprehensive evaluation.
+                    mention the resons for the recommendation advice.
+                    all the needed information are available in {content}
+                """
             ),
             HumanMessage(content="{request}"),
         ]
