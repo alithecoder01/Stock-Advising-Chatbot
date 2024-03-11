8a3fd3b41f35c212d9cfe5e2b3320991d7c5bbb2
Ali Hasan <capt.3lii@gmail.com>
2024-03-11 22:27:21 +0300

    last update with the memory

--- analysing.py
+++ analysing.py
@@ -8,7 +8,7 @@ from langchain.memory import ConversationBufferMemory
 filePath = "/Users/3lihasan/Documents/UNI/499/test.txt"
 
 
-def analyse(request):
+def analyse(request, history):
 
     messages = [
         SystemMessage(
@@ -55,18 +55,22 @@ def analyse(request):
     argument = name_ticker_response.get("additional_kwargs")["function_call"][
         "arguments"
     ]
+
     if argument:
         # Parse the arguments from a JSON string to a Python dictionary
-        argument = json.loads(argument)
-        company_name = argument["company_name"]
-        company_ticker = argument["company_ticker"]
         try:
-            period = argument["period"]
+            argument = json.loads(argument)
+            company_name = argument["company_name"]
+            company_ticker = argument["company_ticker"]
+            try:
+                period = argument["period"]
 
+            except:
+                period = "1y"
+
+            get_all_data(company_name, company_ticker, period, filePath)
         except:
-            period = "1y"
-
-        get_all_data(company_name, company_ticker, period, filePath)
+            print("Not Founded")
 
         with open(filePath, "r") as file:
             content = file.read()[:14000]
@@ -74,29 +78,40 @@ def analyse(request):
         model = ChatOpenAI(model="gpt-3.5-turbo", temperature=1)
         messages = [
             SystemMessage(
-                content=f"""Conduct a concise analysis that includes financial data—such as Stock Evolution, balance sheet, cash flow statement, and income statement—and key valuation measures like P/E, P/B ratios, and dividend yield for {company_name}.
+                content=f""" Answer any question that the user ask about.
+                
+                    If the User asked for analysis or if he should buy the {company_name} stock do this :
+                    Conduct a concise analysis that includes financial data—such as Stock Evolution, balance sheet, cash flow statement, and income statement—and key valuation measures like P/E, P/B ratios, and dividend yield for {company_name}.
                     Assess the financial health, operational performance, and market valuation, integrating an evaluation of recent news to identify significant opportunities and risks.
                     Summarize the findings to provide a clear investment recommendation advice, indicating whether investing in {company_name} is favorable or if caution is advised due to identified risks.
                     Your analysis should offer straightforward investment advice based on the comprehensive evaluation.
                     mention the resons for the recommendation advice.
-                    all the needed information are available in {content}
+
+                    All the needed information are available in {content} .
+
                 """
             ),
             HumanMessage(content="{request}"),
         ]
+        mem = ConversationBufferMemory()
 
-        chain = ConversationChain(
-            llm=model, memory=ConversationBufferMemory(), verbose=True
-        )
+        for i in range(0, len(history), 2):
+            mem.save_context({"input": history[i]}, {"output": history[i+1]})
+
+        chain = ConversationChain(llm=model, memory=mem, verbose=True)
 
         respons = chain.invoke(messages)
+        # chain.predict(input=request)
 
         
-        # analys_response = model.invoke(messages).dict()
-        # return analys_response.get("content")
 
         return respons.get("response")
 
 
-user = input()
-print(analyse(user))
+messages = []
+while True:
+    user = input()
+    result = analyse(user, messages)
+    print(result)
+    messages.append(user)
+    messages.append(result)
