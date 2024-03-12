630052ccb1bffb529c86aa714de241be235df464
Ali Hasan <capt.3lii@gmail.com>
2024-03-12 17:48:52 +0300

    Added test file to test the memory.

--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 import json
 from langchain_openai import ChatOpenAI
-from langchain.schema import HumanMessage, SystemMessage
+from langchain.schema import HumanMessage, SystemMessage,AIMessage
 from get_all_data import get_all_data
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
@@ -8,8 +8,7 @@ from langchain.memory import ConversationBufferMemory
 filePath = "/Users/3lihasan/Documents/UNI/499/test.txt"
 
 
-def analyse(request, history):
-
+def analyse(request):
     messages = [
         SystemMessage(
             content="You are a helpful assistant that find the name of the company and the stock ticker of it and you must give just the company name and the stock ticke. provide the period needed, if not mentioned in the request make it 1y by defaultr"
@@ -68,7 +67,7 @@ def analyse(request, history):
             except:
                 period = "1y"
 
-            get_all_data(company_name, company_ticker, period, filePath)
+            # get_all_data(company_name, company_ticker, period, filePath)
         except:
             print("Not Founded")
 
@@ -76,39 +75,34 @@ def analyse(request, history):
             content = file.read()[:14000]
 
         model = ChatOpenAI(model="gpt-3.5-turbo", temperature=1)
-        messages = [
+        message = [
             SystemMessage(
-                content=f""" Answer any question that the user ask about.
-                
-                    If the User is asking somehting related to a stock do this :
-                    Conduct a concise analysis that includes financial data—such as Stock Evolution, balance sheet, cash flow statement, and income statement—and key valuation measures like P/E, P/B ratios, and dividend yield for {company_name}.
-                    Assess the financial health, operational performance, and market valuation, integrating an evaluation of recent news to identify significant opportunities and risks.
-                    Summarize the findings to provide a clear investment recommendation advice, indicating whether investing in {company_name} is favorable or if caution is advised due to identified risks.
-                    Your analysis should offer straightforward investment advice based on the comprehensive evaluation.
-                    mention the resons for the recommendation advice based on the question.
-                    All the needed information for the analysis are available in {content} .
-
-                """
+                content="You are a helpful assistant"
             ),
-            HumanMessage(content="{request}"),
+            HumanMessage(content="Hi"),
+            AIMessage(content="Hello, How can i help you?"),
+            HumanMessage(content=request)
         ]
+        respns = model.invoke(message).content
+        
+        while True:
+            message.append(respns)
+            
+
+        return respns
         mem = ConversationBufferMemory()
 
-        for i in range(0, len(history), 2):
-            mem.save_context({"input": history[i]}, {"output": history[i+1]})
+        # chain = ConversationChain(llm=model, memory=mem, verbose=True)
 
-        chain = ConversationChain(llm=model, memory=mem, verbose=True)
-
-        respons = chain.invoke(messages)
+        # respons = chain.invoke(messages)
         # chain.predict(input=request)
 
-        return respons.get("response")
+        # return respons.get("response")
 
 
-messages = []
+
 while True:
     user = input()
-    result = analyse(user, messages)
+    result = analyse(user)
     print(result)
-    messages.append(user)
-    messages.append(result)
+    
