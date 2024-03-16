3d3501c60cc7afbe099aabb0ad431bf00d2031b6
Ali Hasan <capt.3lii@gmail.com>
2024-03-16 21:26:41 +0300

    update the analysis

--- testMe.py
+++ testMe.py
@@ -8,12 +8,9 @@ filePath = "/Users/3lihasan/Documents/UNI/499/test.txt"
 
 
 def analyse():
-    message = [
-        SystemMessage(content="Answer any question that the user ask."),
-    ]
-    
-    while True:
-        request = input()
+    request = input()
+
+    while request!="bye":
         messages = [
             SystemMessage(
                 content="You are a helpful assistant that find the name of the company and the stock ticker of it and you must give just the company name and the stock ticke. provide the period needed, if not mentioned in the request make it 1y by defaultr"
@@ -37,10 +34,7 @@ def analyse():
                         "type": "string",
                         "description": "the ticker of the stock of the company",
                     },
-                    "period": {
-                        "type": "string",
-                        "description": "The period of analysis",
-                    },
+                    "period": {"type": "string", "description": "The period of analysis"},
                     "filename": {
                         "type": "string",
                         "description": "the filename to store data",
@@ -53,9 +47,7 @@ def analyse():
         function_call = {"name": "get_all_data"}
 
         model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.9)
-        model = model.bind(
-            functions=[dict(functions)], function_call=dict(function_call)
-        )
+        model = model.bind(functions=[dict(functions)], function_call=dict(function_call))
 
         name_ticker_response = model.invoke(messages).dict()
 
@@ -85,6 +77,21 @@ def analyse():
                 content = file.read()[:14000]
 
             model = ChatOpenAI(model="gpt-3.5-turbo", temperature=1, verbose=True)
+            message = [
+                SystemMessage(
+                    content=f"""You are a stock advisor.
+                        
+                            Answer any question that the user ask.
+                        
+                            If the User is asking somehting related to a stock do this :
+                            Conduct a concise analysis that includes financial data—such as Stock Evolution, balance sheet, cash flow statement, and income statement—and key valuation measures like P/E, P/B ratios, and dividend yield for {company_name}.
+                            Assess the financial health, operational performance, and market valuation, integrating an evaluation of recent news to identify significant opportunities and risks.
+                            Summarize the findings to provide a clear investment recommendation advice, indicating whether investing in {company_name} is favorable or if caution is advised due to identified risks.
+                            Your analysis should offer straightforward investment advice based on the comprehensive evaluation.
+                            
+                            ."""
+                ),
+            ]
 
             
             promp = HumanMessage(content=request)
@@ -92,9 +99,9 @@ def analyse():
             respns = model.invoke(message).content
             ai = AIMessage(content=respns)
             message.append(ai)
-            
             print(message)
             print(respns)
+            request = input()
 
 
 analyse()
