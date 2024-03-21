bdad5a26f69aae6e0f4f138317ebbde1533463bd
Ali Hasan <capt.3lii@gmail.com>
2024-03-22 02:59:49 +0300

    testing file, added the content of the stock with the user request, this will help the model with having the knowledge

--- testing.py
+++ testing.py
@@ -66,7 +66,7 @@ def analyse(request, History):
             except:
                 period = "1y"
 
-            # get_all_data(company_name, company_ticker, period, filePath)
+            get_all_data(company_name, company_ticker, period, filePath)
         except:
             print("Not Founded")
 
@@ -76,30 +76,37 @@ def analyse(request, History):
         model = ChatOpenAI(model="gpt-3.5-turbo", temperature=1, verbose=True)
         message = [
             SystemMessage(
-                content=f"""
-                    Answer any question that the user ask.
-                """
+                content=f"""You are a stock advisor.
+                        
+                            Answer any question that the user ask.
+                        
+                            If the User is asking somehting related to a stock do this :
+                            Conduct a concise analysis that includes financial data—such as Stock Evolution, balance sheet, cash flow statement, and income statement—and key valuation measures like P/E, P/B ratios, and dividend yield for {company_name}.
+                            Assess the financial health, operational performance, and market valuation, integrating an evaluation of recent news to identify significant opportunities and risks.
+                            Summarize the findings to provide a clear investment recommendation advice, indicating whether investing in {company_name} is favorable or if caution is advised due to identified risks.
+                            Your analysis should offer straightforward investment advice based on the comprehensive evaluation.
+                            All the needed information for the analysis are available in {content} .
+                            """
             ),
-            
         ]
         for i in range(0, len(History), 2):
             prompt = HumanMessage(content=History[i])
             message.append(prompt)
-            response = AIMessage(content=History[i+1])
+            response = AIMessage(content=History[i + 1])
             message.append(response)
 
-        prompt = HumanMessage(content=request)
+        prompt = HumanMessage(content=request + content)
         message.append(prompt)
         respns = model.invoke(message).content
         print(message)
         return respns
 
 
+messages = []
 while True:
-    messages = []
+
     request = input()
     response = analyse(request, messages)
     print(response)
     messages.append(request)
     messages.append(response)
-
