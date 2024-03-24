d75c6860986c9a3b026e62462ee29fd125f21085
Ali Hasan <capt.3lii@gmail.com>
2024-03-24 22:00:36 +0300

    last update

--- api.py
+++ api.py
@@ -1,31 +1,16 @@
 from flask import Flask, jsonify, request
 # from analysing import analyse
-from testing import analyse
+from main import analyse
 
 app = Flask(__name__)
 
-@app.route('/hello/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 async def welcome():
-    messages = []
-    print("1")
-    requestt = request.args.get('name', default='hello', type=str)
-    print(requestt)
-    response = analyse(requestt, messages)
-    messages.append(requestt)
-    messages.append(response)
+    
+    requestt = request.get_json()
+    response = analyse(requestt['query'], requestt['history'])
+    
     return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
-
-# app = Flask(__name__)
-
-# @app.route('/<int:number>/')
-# def incrementer(number):
-#     return "Incremented number is " + str(number+1)
-
-# @app.route('/<string:name>/')
-# def hello(name):
-#     return "Hello " + name
-
-# app.run()
