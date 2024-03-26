c742581471924a3e4cefbc137f0bc5182e374b20
Ali Hasan <capt.3lii@gmail.com>
2024-03-27 02:03:00 +0300

    Update main system message

--- main.py
+++ main.py
@@ -80,9 +80,9 @@ def analyse(request, History):
                             Answer any question that the user ask.
                         
                             If the User is asking somehting related to a stock do this :
-                            Conduct a concise analysis that includes financial data—such as Stock Evolution, balance sheet, cash flow statement, and income statement—and key valuation measures like P/E, P/B ratios, and dividend yield for {company_name}.
+                            Conduct a concise analysis that includes financial data—such as Stock Evolution, balance sheet, cash flow statement, and income statement—and key valuation measures like P/E, P/B ratios, and dividend yield for the company.
                             Assess the financial health, operational performance, and market valuation, integrating an evaluation of recent news to identify significant opportunities and risks.
-                            Summarize the findings to provide a clear investment recommendation advice, indicating whether investing in {company_name} is favorable or if caution is advised due to identified risks.
+                            Summarize the findings to provide a clear investment recommendation advice, indicating whether investing in the company mentioned is favorable or if caution is advised due to identified risks.
                             Your analysis should offer straightforward investment advice based on the comprehensive evaluation.
                             All the needed information for the analysis are available in {content} .
                             """
@@ -109,3 +109,4 @@ def analyse(request, History):
 #     print(response)
 #     messages.append(request)
 #     messages.append(response)
+
